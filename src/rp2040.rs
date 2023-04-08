a4aea40a98b9ec4e287c665f351023722d2bba27
John Aughey <jha@aughey.com>
2023-04-08 19:32:52 +0000

    works!

--- src/rp2040.rs
+++ src/rp2040.rs
@@ -53,6 +53,16 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
         // Load up a new state machine to run the 256fs clock
         // Define some simple PIO program.
 
+        // All state machines will run at this 256fs system clock
+        // and do delays of [3].
+        let (system_clock_int, system_clock_frac) = {
+            let system_clock: f64 = clocks.system_clock.freq().to_Hz() as f64;
+            let fsclock: f64 = 256.0 * 44100.0 * 2.0;
+            let int: u16 = (system_clock / fsclock) as u16;
+            let frac: u8 = ((system_clock / fsclock - int as f64) * 256.0) as u8;
+            (int, frac)
+        };
+
         // PIO0
         let tx = {
             // Initialize and start PIO
@@ -70,17 +80,17 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
                         "                    ;        |/-- BCLK",
                         ".wrap_target        ;        ||",
                         "bitloop1:           ;        ||",
-                        "    out pins, 1       side 0b10",
-                        "    jmp x-- bitloop1  side 0b11",
-                        "    out pins, 1       side 0b00",
-                       "    set x, 30         side 0b01",
+                        "    out pins, 1       side 0b10 [3]",
+                        "    jmp x-- bitloop1  side 0b11 [3]",
+                        "    out pins, 1       side 0b00 [3]",
+                       "    set x, 30          side 0b01 [3]",
                         "",
                         "bitloop0:",
-                        "    out pins, 1       side 0b00",
-                        "    jmp x-- bitloop0  side 0b01",
-                        "    out pins, 1       side 0b10",
+                        "    out pins, 1       side 0b00 [3]",
+                        "    jmp x-- bitloop0  side 0b01 [3]",
+                        "    out pins, 1       side 0b10 [3]",
                         "public entry_point:",
-                        "    set x, 30         side 0b11",
+                        "    set x, 30         side 0b11 [3]",
                         ".wrap"
                         options(max_program_size = 32) // Optional, defaults to 32
                     );
@@ -89,20 +99,20 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
                 pio.install(&program_output).unwrap()
             };
 
-            let (int, frac) = {
-                let system_clock: f64 = clocks.system_clock.freq().to_Hz() as f64;
-                let bit_freq: f64 = 44100.0 * 32.0 * 2.0 * 2.0;
-                let int: u16 = (system_clock / bit_freq) as u16;
-                let frac: u8 = ((system_clock / bit_freq - int as f64) * 256.0) as u8;
-                (int, frac)
-            };
+            // let (int, frac) = {
+            //     let system_clock: f64 = clocks.system_clock.freq().to_Hz() as f64;
+            //     let bit_freq: f64 = 44100.0 * 32.0 * 2.0 * 2.0;
+            //     let int: u16 = (system_clock / bit_freq) as u16;
+            //     let frac: u8 = ((system_clock / bit_freq - int as f64) * 256.0) as u8;
+            //     (int, frac)
+            // };
 
             let (mut sm, _, tx) = bsp::hal::pio::PIOBuilder::from_program(installed_writer)
                 .out_pins(9, 3) // I2S data pin
                 .side_set_pin_base(10) // I2S Clock Pin
                 .autopull(true)
                 .pull_threshold(32)
-                .clock_divisor_fixed_point(int, frac)
+                .clock_divisor_fixed_point(system_clock_int, system_clock_frac)
                 .build(sm0);
 
             sm.set_pindirs(
@@ -121,46 +131,32 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
         let rx = {
             let (mut pio, sm0, sm1, _, _) = pac.PIO1.split(&mut pac.RESETS);
             // 256fs clock
-            // Both state machines share the same clock because we want the
-            // system clock and the i2s master (reader) to be synchronized
-            let (int, frac) = {
-                let installed_system_clock = {
-                    let program_system_clock = {
-                        let program_with_defines = pio_proc::pio_asm!(
-                            ".wrap_target          ",
-                            "    set pins, 1       ",
-                            "    set pins, 0       ",
-                            ".wrap"
-                            options(max_program_size = 32) // Optional, defaults to 32
-                        );
-                        program_with_defines.program
-                    };
-                    pio.install(&program_system_clock).unwrap()
+
+            let installed_system_clock = {
+                let program_system_clock = {
+                    let program_with_defines = pio_proc::pio_asm!(
+                        ".wrap_target          ",
+                        "    set pins, 1       ",
+                        "    set pins, 0       ",
+                        ".wrap"
+                        options(max_program_size = 32) // Optional, defaults to 32
+                    );
+                    program_with_defines.program
                 };
-
-                let (int, frac) = {
-                    let system_clock: f64 = clocks.system_clock.freq().to_Hz() as f64;
-                    let fsclock: f64 = 256.0 * 44100.0 * 2.0;
-                    let int: u16 = (system_clock / fsclock) as u16;
-                    let frac: u8 = ((system_clock / fsclock - int as f64) * 256.0) as u8;
-                    (int, frac)
-                };
-
-                let _pin5 = pins.gpio5.into_mode::<bsp::hal::gpio::FunctionPio1>();
-
-                let (mut sm, _, _) =
-                    bsp::hal::pio::PIOBuilder::from_program(installed_system_clock)
-                        // .out_pins(8, 1) // High Frequency I2S Sys Clock
-                        //.side_set_pin_base(10) // I2S Clock Pin
-                        //.autopull(true)
-                        .set_pins(5, 1)
-                        .clock_divisor_fixed_point(int, frac)
-                        .build(sm0);
-                sm.set_pindirs([(5, bsp::hal::pio::PinDir::Output)].into_iter());
-                sm.start();
-
-                (int, frac)
+                pio.install(&program_system_clock).unwrap()
             };
+
+            let _pin5 = pins.gpio5.into_mode::<bsp::hal::gpio::FunctionPio1>();
+
+            let (mut sm, _, _) = bsp::hal::pio::PIOBuilder::from_program(installed_system_clock)
+                // .out_pins(8, 1) // High Frequency I2S Sys Clock
+                //.side_set_pin_base(10) // I2S Clock Pin
+                //.autopull(true)
+                .set_pins(5, 1)
+                .clock_divisor_fixed_point(system_clock_int, system_clock_frac)
+                .build(sm0);
+            sm.set_pindirs([(5, bsp::hal::pio::PinDir::Output)].into_iter());
+            sm.start();
 
             // Receive
             let rx = {
@@ -178,17 +174,17 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
                             ".wrap_target        ;        ||",
                             "bitloop1:           ;        ||",
                             // A delay of 4 throughout because we run on the same clock as the system clock
-                            "    in pins, 1       side 0b10 [4]",
-                            "    jmp x-- bitloop1 side 0b11 [4]",
-                            "    in pins, 1       side 0b00 [4]",
-                           "    set x, 30         side 0b01 [4]",
+                            "    in pins, 1       side 0b10 [3]",
+                            "    jmp x-- bitloop1 side 0b11 [3]",
+                            "    in pins, 1       side 0b00 [3]",
+                           "    set x, 30         side 0b01 [3]",
                             "",
                             "bitloop0:",
-                            "    in pins, 1       side 0b00 [4]",
-                            "    jmp x-- bitloop0 side 0b01 [4]",
-                            "    in pins, 1       side 0b10 [4]",
+                            "    in pins, 1       side 0b00 [3]",
+                            "    jmp x-- bitloop0 side 0b01 [3]",
+                            "    in pins, 1       side 0b10 [3]",
                             "public entry_point:",
-                            "    set x, 30        side 0b11 [4]",
+                            "    set x, 30        side 0b11 [3]",
                             ".wrap"
                             options(max_program_size = 32) // Optional, defaults to 32
                         );
@@ -203,7 +199,7 @@ pub fn initialize_pio_state_machines() -> (impl FnMut() -> u32, impl FnMut(u32),
                     .side_set_pin_base(7) // I2S Clock Pin
                     .autopush(true)
                     .push_threshold(32)
-                    .clock_divisor_fixed_point(int, frac)
+                    .clock_divisor_fixed_point(system_clock_int, system_clock_frac)
                     .build(sm1);
 
                 sm.set_pindirs(
